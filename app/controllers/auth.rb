5548d6b394bf514e118bd548aebf7cd14aefefbb
Thierry <marcelinthierry@gmail.com>
2022-08-06 13:35:27 +0800

    Add views to project

---
+++ app/controllers/auth.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'roda'
+require_relative './app'
+
+module TrackIt
+  # Web controller for TrackIt API
+  class App < Roda
+    route('auth') do |routing|
+      @login_route = '/auth/login'
+      routing.public
+      routing.is 'login' do
+        # GET /auth/login
+        routing.get do
+          view :login
+        end
+
+        # POST /auth/login
+        routing.post do
+          binding.pry
+          account = AuthenticateAccount.new(App.config).call(
+            username: routing.params['username'],
+            password: routing.params['password']
+          )
+
+          session[:current_account] = account
+          flash[:notice] = "Welcome back #{account['username']}!"
+          routing.redirect '/'
+        rescue StandardError
+          flash.now[:error] = 'Username and password did not match our records'
+          response.status = 400
+          view :login
+        end
+      end
+
+      routing.on 'logout' do
+        routing.get do
+          session[:current_account] = nil
+          routing.redirect @login_route
+        end
+      end
+    end
+  end
+end
